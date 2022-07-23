ae05aa121780b0d5ed82795e4cc9533c031600ac
lipan padhan <lipanpadhan1432@gmail.com>
2022-07-23 23:01:16 +0530

    adding reduce

--- Advance JS practise/destructuring.js
+++ Advance JS practise/destructuring.js
@@ -20,49 +20,64 @@
 // }
 // printValue(car)
 
-
-
-
 // // Template string
 
 // const age = 23;
 // const name = "lipan";
 // // const str = "Hi my name is " + name + " and i am " + age + " years old";
 
-
-// // this is template string 
+// // this is template string
 // const str = `Hi my name is ${name} and i am ${age} years old`
 // console.log(str);
 
-
-
-
-
-// Function type 
-
+// Function type
 
 // 1. normal function
 //  function add (a,b){
 //     return a+b
 //  }
 
-
-// 2. obj function 
+// 2. obj function
 //   const add = function (a,b){
-//     return  a+b 
+//     return  a+b
 //   }
 
-
-// 3. Arrow Function 
-// const add = (a,b) =>{ 
-//    return a+b 
+// 3. Arrow Function
+// const add = (a,b) =>{
+//    return a+b
 // }
 // console.log(add(2,5));
 
+// map in array
+// const normalArray = [1,2,3,4];
+//2 3 4 5
 
-const normal = [1,2,3,4]
+// const mapArray = normalArray.map((num)=>{
+//     return num+1;
+// })
+// console.log(mapArray);
 
-const mapArray = normal.map((num)=>{
-    return num+1;
-})
-console.log(mapArray);
+// forEach loop in array
+// const normalArray = [1,2,3,4];
+// const double=[]
+// normalArray.forEach((num)=>{
+//  double.push(num+1)
+// }) ;
+
+//  console.log(double);
+
+// filter in js
+// const normalArray = [1, 2, 3, 4, 5, 6, 7];
+// const filterArray = normalArray.filter((num) => {
+//   return num > 2 && num < 7;
+// });
+
+// console.log(filterArray);
+
+// reduce in js 
+// const normalArray = [1, 2, 3, 4, 5, 6 ];
+// const reduceArray = normalArray.reduce((accumulator,num)=>{
+//     return accumulator + num ; //accumulator = accumulator + num // 1 3 6 10 15 21 //dry run
+// }, 0 ) ; // <- initialize 
+
+// console.log(reduceArray);
